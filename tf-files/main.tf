50e5351038f4f09c797280479c18d1e524a1950d
matthew newbould <mjn.97@outlook.com>
2026-09-09 15:50:14 +0100

    added iam role and policy for lambda to interract with dynamo_db

--- tf-files/main.tf
+++ tf-files/main.tf
@@ -21,7 +21,61 @@ data "aws_subnets" "private" {
 }
 
 
+# ---- IAM ROLE POLICY -----
 
+resource "aws_iam_role" "lambda_exec" {
+    name = "hosp_proxy_lambda_execution_role"
 
+# Trust Policy: Allows Lambda service to assume this role
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+        Service   = "lambda.amazonaws.com"
+        }
+        }
+        ]
+    })
+}
+
+# ---- IAM IDENTITY POLICY ----
+
+resource "aws_iam_policy" "lambda_dynamodb_cache" {
+    name        = "hosp_proxy_dynamodb_cache_policy"
+    description = "Allows proxy Lambda to read, write, and invalidate DynamoDB cache items"
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Effect = "Allow"
+            Action = [
+                "dynamodb:GetItem",
+                "dynamodb:PutItem",
+                "dynamodb:DeleteItem"
+            ]
+            Resource = aws_dynamodb_table.api_cache.arn # Scoped strictly to your cache table
+        },
+        {
+            Effect = "Allow"
+            Action = [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ]
+            Resource = "arn:aws:logs:*:*:*"
+        }
+        ]
+    })
+}
+
+# Attach Policy to the Role
+resource "aws_iam_role_policy_attachment" "attach_cache_policy" {
+    role       = aws_iam_role.lambda_exec.name
+    policy_arn = aws_iam_policy.lambda_dynamodb_cache.arn
+}
 
 # ---- LAMBDA
